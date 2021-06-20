9e92d0d353069f7eb1dd5f05f9e3324ee0c722a3
ShawonAshraf <shawon13@live.com>
2021-06-20 08:36:07 +0200

    resnet18 works

--- classfier.py
+++ classfier.py
@@ -13,20 +13,33 @@ class FrutifyClassifier(pl.LightningModule):
     def __init__(self, num_labels, learning_rate):
         super(FrutifyClassifier, self).__init__()
 
+        self.save_hyperparameters()
+
         self.num_labels = num_labels
         self.learning_rate = learning_rate
 
-        self.model = torchvision.models.inception_v3(pretrained=True,
-                                                     progress=True,
-                                                     transform_input=True)
-        num_features = self.model.fc.in_features
+        self.feature_model = torchvision.models.resnet18(pretrained=True, progress=True)
+        self.feature_model.eval()
+        # freeze params
+        for param in self.feature_model.parameters():
+            param.requires_grad = False
 
-        self.model.fc = nn.Linear(num_features, self.num_labels)
-        self.softmax = nn.Softmax()
+        n_features = self.__find_n_features()
+
+        self.classifier = nn.Linear(n_features, num_labels)
+
+    def __find_n_features(self):
+        inp = torch.autograd.Variable(
+            torch.rand(1, 3, 299, 299)
+        )
+        out_f = self.feature_model(inp)
+        n_size = out_f.data.view(1, -1).size(1)
+        return n_size
 
     def forward(self, x):
-        out, _ = self.model(x)
-        out = self.softmax(out)
+        out = self.feature_model(x)
+        out = out.view(out.size(0), -1)
+        out = F.log_softmax(self.classifier(out), dim=1)
         return out
 
     def configure_optimizers(self):
@@ -36,7 +49,7 @@ class FrutifyClassifier(pl.LightningModule):
         image, label = batch["image"], batch["label"]
 
         out = self.forward(image)
-        loss = F.cross_entropy(out, label)
+        loss = F.nll_loss(out, label)
 
         return {
             "loss": loss
@@ -46,7 +59,7 @@ class FrutifyClassifier(pl.LightningModule):
         image, label = batch["image"], batch["label"]
 
         out = self.forward(image)
-        loss = F.cross_entropy(out, label)
+        loss = F.nll_loss(out, label)
 
         # log validation loss to progress bar
         self.log('validation_loss', loss, prog_bar=True)
