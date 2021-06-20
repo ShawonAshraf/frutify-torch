1b6d7e39a064e4364a98703a28b422ba8c7494b0
ShawonAshraf <shawon13@live.com>
2021-06-20 03:05:49 +0200

    utils for images

--- image_utils.py
+++ image_utils.py
@@ -6,3 +6,11 @@ def generate_labels(fruit_names):
         labels.append("rotten_" + fruit_name)
 
     return labels
+
+
+def get_label_from_file_name(file_name):
+    splits = file_name.split("_")
+    # file name example: fresh_orange_1.jpg
+    # the label is the first two tokens joined by _
+    label = "_".join(s for s in splits[:-1])
+    return label
