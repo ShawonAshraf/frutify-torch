c87191b2bdb69f185c1224d7cd92283101989e04
ShawonAshraf <shawon13@live.com>
2021-06-20 04:25:59 +0200

    train test dev split

--- image_utils.py
+++ image_utils.py
@@ -1,4 +1,8 @@
 import os
+import math
+import random
+
+import torch.utils.data as D
 
 IMAGE_FOLDER_NAME = "dataset"
 IMAGE_DIR_ROOT = os.path.join(os.getcwd(), IMAGE_FOLDER_NAME)
@@ -26,3 +30,27 @@ def get_label_from_file_name(file_name):
 
 def get_all_file_names(root_dir=IMAGE_DIR_ROOT):
     return os.listdir(root_dir)
+
+
+# train_ratio is a float number
+# https://stackoverflow.com/a/61818182/3316525
+
+# returns 3 lists, containing image names for train, validation and test sets
+def create_train_test_dev_set(all_file_names, train_ratio):
+    total_len = len(all_file_names)
+
+    train_len = int(train_ratio * total_len)
+    val_len = int((1 - train_ratio) * total_len)
+    test_len = total_len - train_len - val_len
+
+    train_set, val_set, test_set = D.random_split(
+        all_file_names, (train_len, val_len, test_len)
+    )
+
+    # random split returns a torch dataset
+    # but strings are required here!
+    train_set = [str(i) for i in train_set]
+    test_set = [str(i) for i in test_set]
+    val_set = [str(i) for i in val_set]
+
+    return train_set, val_set, test_set
